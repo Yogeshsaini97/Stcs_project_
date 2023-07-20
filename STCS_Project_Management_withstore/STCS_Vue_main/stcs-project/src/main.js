2d61f0abf65cd7d6ab190dbf7042bef808b0cef1
Yogesh saini <84733974+Yogeshsaini97@users.noreply.github.com>
2023-07-20 12:34:29 +0530

    Remover previous router, added store vuex file

--- STCS_Project_Management_withstore/STCS_Vue_main/stcs-project/src/main.js
+++ STCS_Project_Management_withstore/STCS_Vue_main/stcs-project/src/main.js
@@ -1,8 +1,9 @@
 import App from './App.ce.vue'
 import ProgressBar from './Components/Global/ProgressBar.ce.vue'
 import { defineCustomElement } from './defineCustomElementWithStyles'
+import store from './store'
 
 customElements.define(
   'stcs-project',defineCustomElement(App, {
-    // plugins: [store,router] 
+    plugins: [store] 
    } ,{ProgressBar}))
